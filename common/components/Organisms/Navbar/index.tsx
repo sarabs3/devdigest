f555345561467c9ef7c428f3bbc67161de57aa89
sarabjeet singh <sarabsingh212@gmail.com>
2024-01-05 09:26:51 +0530

    Add Navbar component to layout

---
+++ common/components/Organisms/Navbar/index.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className={`p-4 border-b-2`}>
+      <div className={`container mx-auto`}>
+        <h2 className={`m-0 text-2xl font-semibold`}>Dev Digest</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
